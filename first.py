df17946cfcf3649954c9942c71d80f9f0b6826eb
skbss1224 <73568092+skbss1224@users.noreply.github.com>
2022-11-15 15:52:18 +0530

    first
    python

---
+++ first.py
@@ -0,0 +1,32 @@
+'''print("hello sasi")
+print("hello sanjai")
+print("hello vijay")
+print("hello mani")
+print("hello baskar")
+print("hello vino")'''
+#print("iam sasi")
+
+
+'''number=5---int
+float=45.89766---float
+str=jhahdhjav-string'''
+
+'''sasi=10
+moulitharan=34
+print(sasi+moulitharan)'''
+'''
+artithmetic operator
+
++---plus operator
+- ---minus operator
+* ----mul operator
+/ ---div operator
+% ---per operator'''
+
+
+manoj_kumar=20000
+less=4000
+baby=2000
+home=1000
+balance_amt=20000-4000-2000-1000
+print(balance_amt)
